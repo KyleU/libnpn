cf092119de623ca3999ae58a0de8dda22a797855
Kyle Unverferth <kyle@kyleu.com>
2021-01-06 19:58:06 -0700

    String method

--- npncore/string.go
+++ npncore/string.go
@@ -27,3 +27,18 @@ func SplitStringLast(s string, sep byte, cutc bool) (string, string) {
 	}
 	return s[:i], s[i:]
 }
+
+// Splits a string according to a delimeter, then trims each entry, filtering empty strings
+func SplitAndTrim(s string, delim string) []string {
+	split := strings.Split(s, delim)
+	ret := make([]string, 0, len(split))
+
+	for _, x := range split {
+		x = strings.TrimSpace(x)
+		if len(x) > 0 {
+			ret = append(ret, x)
+		}
+	}
+
+	return ret
+}
